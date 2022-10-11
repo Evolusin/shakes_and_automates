f7dbb51630a7bfb3d9f7f36c55acb030a523fb0f
Evolusin <98921577+Evolusin@users.noreply.github.com>
2022-10-11 22:20:18 +0200

    Search for Q Npc, auto accept Quest + more png

--- run.py
+++ run.py
@@ -7,8 +7,11 @@ import pyautogui
 
 config = Settings()
 state = 0
-print(pyautogui.size())
+# DEBUG MOUSE POS
+# print(pyautogui.position())
 while True:
+    # DEBUG MOUSE POS
+    print(pyautogui.position()) 
     if state == 0:
         login_position = needle_position(config.login_needle)
         if login_position:
@@ -23,10 +26,11 @@ while True:
             click_point(x,y)
             state = 2
     elif state == 2:
-        x = config.width/1.91
-        y = config.height/1.53
-        click_point(x,y)
-        print("Ok czas na wybór misji")
+        print("Jestem w karczmie. Przechodzę do klikania npc od questów")
+        click_point(config.karczma_questnpc1_x,config.karczma_questnpc1_y)
+        click_point(config.karczma_questnpc2_x,config.karczma_questnpc2_y)
+        print("Akceptuję misję")
+        click_point(config.karczma_quest_x,config.karczma_quest_y)
         break
     
     if cv.waitKey(1) == ord("q"):
--- settings.py
+++ settings.py
@@ -8,4 +8,9 @@ class Settings:
             self.monitor = {"top": 0, "left": 0, "width": self.width, "height": self.height}
             self.login_needle = f'{self.img_dir}login.png'
             self.karczma_needle = f'{self.img_dir}karczma.png'
-
+            self.karczma_questnpc1_x = self.width/1.91
+            self.karczma_questnpc1_y = self.height/1.5
+            self.karczma_questnpc2_x = self.width/1.35
+            self.karczma_questnpc2_y = self.height/1.35
+            self.karczma_quest_x = self.width/1.66
+            self.karczma_quest_y = self.height/1.44
